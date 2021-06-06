33972af39bd07cacba535be182a0d3b515a887db
Jnwolf <jrwolf@crimson.ua.edu>
2021-06-06 01:09:38 -0500

    added some animation

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 console.log("Hello World!");
 
 class Person {
-    constructor(name) {
-      this._name = name;
-    }
-    get name() {
-      return this._name;
-    }
-    set name(updateName) {
-      this._name = updateName;
-    }
+  constructor(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(updateName) {
+    this._name = updateName;
+  }
+}
+
+
+function myFunction(x) {
+  x.classList.toggle("change");
 }

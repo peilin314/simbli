8de34a256daadeb7fae88707fb2990cc0d758d50
peilin314 <peilin314159@gmail.com>
2021-06-18 01:27:03 -0500

    Changed the database reference to the first and last name

--- saveInfo.js
+++ saveInfo.js
@@ -1,5 +1,6 @@
 let firstN = document.getElementById("firstName");
 let lastN = document.getElementById("lastName");
+let gender = document.getElementById("gender");
 let email = document.getElementById("email");
 let pw = document.getElementById("password");
 let confirmPw = document.getElementById("confirmPw");
@@ -15,9 +16,8 @@ const database = firebase.database();
 
 saveBtn.addEventListener('click', (e) => {
 	e.preventDefault();
-	database.ref('/names/' + firstN.value).set({
-	//	firstN: firstN.value,
-		lastN: lastN.value,
+	database.ref('/name/' + firstN.value + lastN.value).set({
+		gender: gender.value,
 		email: email.value,
 		pw: pw.value,
 		phone: phone.value
